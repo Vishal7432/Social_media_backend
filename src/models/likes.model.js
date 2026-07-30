a1571cde6493aa4c5265dcc21ff488f58de5e111
Vishal Gupta <vishalgupta10852@gmail.com>
2026-07-30 18:18:08 +0530

    Add Mongoose models for comments, likes, playlists, and tweets

---
+++ src/models/likes.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const likeSchema = mongoose.Schema(
+  {
+    video: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    comment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Commet",
+    },
+    tweet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    likedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = mongoose.Model("Like", likeSchema);
---
+++ src/models/playlist.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const playListSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    video: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("Playlist", playListSchema);
